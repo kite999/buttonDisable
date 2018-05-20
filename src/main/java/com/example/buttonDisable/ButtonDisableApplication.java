cc64c8642d4d52437a35bd522deb6dafe9ce034b
kite999 <skyfish66@gmail.com>
2018-05-20 13:49:25 +0900

    ロック機能のサンプル　コミットできないものがあったのでコミット

---
+++ src/main/java/com/example/buttonDisable/ButtonDisableApplication.java
@@ -0,0 +1,14 @@
+package com.example.buttonDisable;
+
+import com.example.buttonDisable.thymeleaf.common.SampleDialect;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ButtonDisableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButtonDisableApplication.class, args);
+	}
+}
